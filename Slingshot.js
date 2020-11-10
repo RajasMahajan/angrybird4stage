b5283819d0fd93f7752509c3f0746236a86f7a1e
RajasMahajan <pankajmahajan147@gmail.com>
2020-11-10 17:37:43 +0530

    angry bird 4 stage

--- Slingshot.js
+++ Slingshot.js
@@ -7,6 +7,9 @@ class SlingShot{
             length: 10
         }
         this.pointB = pointB
+        this.image1=loadImage("sprites/sling1.png");
+        this.image2=loadImage("sprites/sling2.png");
+        this.image3=loadImage("sprites/sling3.png");
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
     }
@@ -16,12 +19,28 @@ class SlingShot{
     }
 
     display(){
+       image(this.image1,200,65,50,155);
+       image(this.image2,170,60,50,100);
+    
+
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
             strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            stroke('#301608');
+            if(pointA.x<220){
+                line(pointA.x, pointA.y, pointB.x, pointB.y);
+                line (pointA.x,pointA.y,pointB.x+30,pointB.y);
+                image(this.image3,pointA.x-23,pointA.y-10,15,30);
+            }
+            else{
+                line(pointA.x, pointA.y, pointB.x, pointB.y);
+                line (pointA.x,pointA.y,pointB.x+30,pointB.y);
+                image(this.image3,pointA.x+23,pointA.y-10,15,30);
+            }
+           
         }
+
     }
     
 }
